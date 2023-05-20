e1e27e84f087fcdc738ef496deda207f6a8cb688
Arian Atapour <arian.atapour@student.nhlstenden.com>
2023-05-21 00:29:28 +0200

    Map and Homepage
    Homepage is scuffed af wait for final version

---
+++ mauiMap/mauiMap/mauiMap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mauiMap;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ mauiMap/mauiMap/mauiMap/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace mauiMap;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
