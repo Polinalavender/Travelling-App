fd5266eb229438f0a695a981bcd4c6d6ed5f4789
EvaldN <91316779+EvaldN@users.noreply.github.com>
2023-05-29 07:32:59 +0200

    Committing code from console app, doesnt work

---
+++ MauiApp1/MauiApp1/Image.cs
@@ -0,0 +1,42 @@
+﻿using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace MauiApp1
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=sqldb1", option =>
+            {
+                option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products", "test");
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(k => k.Id);
+                entity.HasIndex(i => i.Name).IsUnique();
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
