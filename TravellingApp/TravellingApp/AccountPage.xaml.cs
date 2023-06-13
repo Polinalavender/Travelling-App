99e7241770e3fa9e82231d7f41f8271868371927
Polina <91316935+Polinalavender@users.noreply.github.com>
2023-06-13 23:06:52 +0200

    add full design and images

---
+++ TravellingApp/TravellingApp/AccountPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace TravellingApp;
+
+
+public partial class AccountPage : ContentPage
+{
+	public AccountPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnHomeImageTapped(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MainPage());
+    }
+
+    private void OnSavedImageTapped(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SavedPage());
+    }
+
+    private void OnMapImageTapped(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new MapPage());
+    }
+
+    private void OnAccountImageTapped(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AccountPage());
+    }
+
+    private void OnFollowButtonClicked(object sender, EventArgs e)
+    {
+        /*if (FollowButton.Text == "Follow")
+        {
+            
+            FollowButton.Text = "Followed";
+            FollowButton.BackgroundColor = Color.Red; 
+        }
+        else
+        {
+            
+            FollowButton.Text = "Follow";
+            FollowButton.BackgroundColor = Color.Blue; 
+        }*/
+    }
+}
