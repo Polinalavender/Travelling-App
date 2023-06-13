6808e7a43b843da24625babdd6c096eb563a58e5
Polina <91316935+Polinalavender@users.noreply.github.com>
2023-06-13 23:14:27 +0200

    pages

---
+++ TravellingApp/TravellingApp/SearchPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace TravellingApp;
+
+public partial class SearchPage : ContentPage
+{
+	public SearchPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnSearchButtonPressed(object sender, EventArgs e)
+    {
+        
+        // Query the database
+    }
+}
+
