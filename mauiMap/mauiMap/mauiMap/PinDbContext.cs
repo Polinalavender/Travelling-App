3496ca37863e5d94b3240fd436f156f0c6e5119f
Arian Atapour <arian.atapour@student.nhlstenden.com>
2023-07-02 20:42:50 +0200

    TESTING MAUI MAPS WITHOUT SQLLITE

---
+++ mauiMap/mauiMap/mauiMap/PinDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using mauiMap.Models; 
+
+namespace mauiMap
+{
+	public class PinDbContext:DbContext
+	{
+        private readonly string databasePath;
+
+        public DbSet<Pin> Pins { get; set; }
+
+        public PinDbContext(string databasePath)
+        {
+            this.databasePath = databasePath;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={databasePath}");
+        }
+
+    }
+}
+
